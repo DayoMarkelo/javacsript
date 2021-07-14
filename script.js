f2b662c5c3f006ae4b397c9ee41b040ea0b8ec35
DayoMarkelo <86260976+DayoMarkelo@users.noreply.github.com>
2021-07-14 14:10:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Hello Winc Academy")
+let name= 'Dayo';
+console.log(name)
+
+let somnumbers = 4+4;
+console.log (somnumbers)
+
+let somstring = '4+4';
+console.log (somstring)
+
+
+let addition = 3+3;
+console.log (addition)
+
+let subtraction = 3-2;
+console.log (subtraction)
+
+let multiplication = 3*2;
+console.log (multiplication)
+
+let division = 6/2;
+console.log (division)
+
+let modulus = 50% 100;
+console.log (modulus)
+
+let leeftijd = 29;
+console.log (typeof leeftijd)
+
+let leeftijdTwee = negenentwintig
+console.log (typeof leeftijdtwee)
