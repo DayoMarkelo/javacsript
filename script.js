2251c68baa5ad4c5f74d7d540f2ef274748e41a8
DayoMarkelo <86260976+DayoMarkelo@users.noreply.github.com>
2021-07-15 18:51:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-console.log("Hallo Winc Academy studenten")
-//Dit is een grote som
+console.log("Hello Winc Academy")
 
-/*let som = 1230941 * 1823794;
-console.log (som)
+const age = 18;
+if (age > 17) {document.write ('Welkom, je mag naar binnen')
+} else if (age < 18) {document.write ('Helaas, je mag niet naar binnen')
+}
 
-let som2 = 637263/54; 
-console.log (som2)*/
+// Deel twee
+const isFemale = true;
+if (isFemale) {document.write ('Welkom bij de ladies night')
+}
+else {document.write ('Je mag niet naar binnen, het is vandaag ladies night')
+}
+
+
+//Deel drie
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {document.write ('Netjes, veilige reis')
+}
+else {document.write ('Bestel een taxi om veilig thuis te komen')}
+
+
