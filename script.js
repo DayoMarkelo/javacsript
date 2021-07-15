f5da3fc25aea41f20c80506cf90ef03876573895
DayoMarkelo <86260976+DayoMarkelo@users.noreply.github.com>
2021-07-15 21:56:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Hello Winc Academy")
-
-const age = 18;
-if (age > 17) {document.write ('Welkom, je mag naar binnen')
-} else if (age < 18) {document.write ('Helaas, je mag niet naar binnen')
+/*
+const ageWelkom = 18;
+if (ageWelkom > 17) {document.write ('Welkom, je mag naar binnen')
+} else if (ageWelkom < 18) {document.write ('Helaas, je mag niet naar binnen')
 }
 
 // Deel twee
@@ -18,5 +18,22 @@ const driverStatus = 'bob';
 if (driverStatus == 'bob') {document.write ('Netjes, veilige reis')
 }
 else {document.write ('Bestel een taxi om veilig thuis te komen')}
+*/
 
+
+//50% korting
+const age = 18;
+if (age >=18 && age <= 25) {console.log ("Je krijgt 50% korting!");}
+else {console.log ("Je krijgt geen korting vanavond");}
+
+// ludieke actie
+const firstName = "Sarah";
+if (firstName == "Bram" || firstName == "Sarah") {console.log ("Je krijgt een gratis biertje");}
+else {console.log ("Dat is dan 2,50");}
+
+//jubileum korting
+const totalAmount = 100;
+if (totalAmount >=25 && totalAmount <=50) {console.log ("gratis (vega)bitterballen");}
+else if (totalAmount >=50 && totalAmount <=10) {console.log ("gratis (vega)bitterballen");}
+else if (totalAmount >=100)  {console.log ("gratis flesje champagne");}
 
