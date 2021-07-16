d0760e5edc68d475fd97050b6f17bc9c8470c3e5
DayoMarkelo <86260976+DayoMarkelo@users.noreply.github.com>
2021-07-16 16:25:22 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,35 +1,29 @@
-
-const makePizza = function() {
-   console.log("Voeg de bloem en gist samen");
-   console.log("Voeg hier water aan toe");
-   console.log("Kneed het deeg");
-};
-
-const makeSushi = function() {
-   console.log("Leg de rijst op het nori vel");
-   console.log("smeer de saus op de rijst");
-   console.log("leg de komkommerreepjes op de saus");
-};
-
-//deel 1
+// No argument
 /*
-makePizza ();
-makePizza ();
-makePizza ();
+const verfKleur = function() {
+   console.log("The wall has been painted red");
+};
+verfKleur ();
 */
 
-//deel 2
 /*
-makeSushi();
-makeSushi();
+//Single argument
+const verfKleur = function(hulpmiddel) {
+console.log("The wall has been painted yellow" + hulpmiddel);
+};
+// without an argument it states the wall has been painted green
+
+verfKleur (" with a brush");
 */
 
+//Multiple arguments
+const wallChoice = function(optie1, optie2) {
+   console.log(optie1 +" has been painted "+ optie2 );
+   };
 
-// Meerdere keren callen zonder te kopieren
-for (var i = 0; i < 3; i++) {
-   makeSushi();
-}
+   const muurKant = "The West wall"
+   const verf_kleur= "turqouise"
 
-for (var i = 0; i < 2; i++) {
-   makePizza();
-}
+   wallChoice (muurKant, verf_kleur)
+   
+
