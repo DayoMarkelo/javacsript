2f3b527a33bb1077470e209248a3b6ab040bd637
DayoMarkelo <86260976+DayoMarkelo@users.noreply.github.com>
2021-07-16 15:07:50 +0200

    Add files via upload
    Opdracht 15

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-console.log("Hello Winc Academy")
+
+const makePizza = function() {
+   console.log("Voeg de bloem en gist samen");
+   console.log("Voeg hier water aan toe");
+   console.log("Kneed het deeg");
+};
+
+const makeSushi = function() {
+   console.log("Leg de rijst op het nori vel");
+   console.log("smeer de saus op de rijst");
+   console.log("leg de komkommerreepjes op de saus");
+};
+
+//deel 1
 /*
-const ageWelkom = 18;
-if (ageWelkom > 17) {document.write ('Welkom, je mag naar binnen')
-} else if (ageWelkom < 18) {document.write ('Helaas, je mag niet naar binnen')
-}
+makePizza ();
+makePizza ();
+makePizza ();
+*/
 
-// Deel twee
-const isFemale = true;
-if (isFemale) {document.write ('Welkom bij de ladies night')
-}
-else {document.write ('Je mag niet naar binnen, het is vandaag ladies night')
-}
-
-
-//Deel drie
-const driverStatus = 'bob';
-if (driverStatus == 'bob') {document.write ('Netjes, veilige reis')
-}
-else {document.write ('Bestel een taxi om veilig thuis te komen')}
+//deel 2
+/*
+makeSushi();
+makeSushi();
 */
 
 
-//50% korting
-const age = 18;
-if (age >=18 && age <= 25) {console.log ("Je krijgt 50% korting!");}
-else {console.log ("Je krijgt geen korting vanavond");}
+// Meerdere keren callen zonder te kopieren
+for (var i = 0; i < 3; i++) {
+   makeSushi();
+}
 
-// ludieke actie
-const firstName = "Sarah";
-if (firstName == "Bram" || firstName == "Sarah") {console.log ("Je krijgt een gratis biertje");}
-else {console.log ("Dat is dan 2,50");}
-
-//jubileum korting
-const totalAmount = 100;
-if (totalAmount >25) {console.log ("gratis (vega)bitterballen");}
-else if (totalAmount >50) {console.log ("gratis (vega)bitterballen");}
-else if (totalAmount >=100)  {console.log ("gratis flesje champagne");}
-else {console.log ("Helaas, geen traktatie");}
+for (var i = 0; i < 2; i++) {
+   makePizza();
+}
