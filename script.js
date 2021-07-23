a20479daa2c2db7fdf8b8e0c42b0fc3cbb713e45
DayoMarkelo <86260976+DayoMarkelo@users.noreply.github.com>
2021-07-23 17:05:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
-
-const makePizza = function() {
-   console.log("Voeg de bloem en gist samen");
-   console.log("Voeg hier water aan toe");
-   console.log("Kneed het deeg");
-};
-
-const makeSushi = function() {
-   console.log("Leg de rijst op het nori vel");
-   console.log("smeer de saus op de rijst");
-   console.log("leg de komkommerreepjes op de saus");
-};
-
-//deel 1
-/*
-makePizza ();
-makePizza ();
-makePizza ();
-*/
-
-//deel 2
-/*
-makeSushi();
-makeSushi();
-*/
-
-
-// Meerdere keren callen zonder te kopieren
-for (var i = 0; i < 3; i++) {
-   makeSushi();
-}
-
-for (var i = 0; i < 2; i++) {
-   makePizza();
-}
+const changeClassBlueBackground = function () {
+    const bodyElement = document.body;
+    bodyElement.classList.add("red-background");
+  };
+  const attachEventToChangeColorButton = function () {
+    const toggleColorButton = document.getElementById("change-background-button");
+    toggleColorButton.addEventListener("click", function () {
+      changeClassBlueBackground();
+    });
+  };
+  const attachEventToAlertButton = function () {
+    const alertButton = document.getElementById("alert-button");
+    alertButton.addEventListener("click", function () {
+      alert("button clicked!");
+    });
+  };
+ 
+  attachEventToAlertButton();
+  attachEventToChangeColorButton();
